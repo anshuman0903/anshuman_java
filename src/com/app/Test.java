39198b5ea989a0c727d40cf68c812fa9334639cc
Anshumman <Anshumman@Anshuma>
2019-01-22 20:17:04 +0530

    yes finally came

---
+++ src/com/app/Test.java
@@ -0,0 +1,12 @@
+package com.app;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String str="anshuman";
+System.out.println(str.toUpperCase());
+System.out.println(str.toLowerCase());
+	}
+
+}
